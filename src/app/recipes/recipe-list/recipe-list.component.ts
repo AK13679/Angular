cf2229647fd37e13bafb192991522ace6ed4a420
Abhishek <akabhishek389@gmail.com>
2017-10-03 19:09:45 +0530

    Added UI modification for components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+ recipes: Recipe[] = [
+   new Recipe('Pizza','Chicken Italiano',
+   'http://www.baltana.com/files/wallpapers-5/Pizza-HD-Desktop-Wallpaper-15280.jpg'),
+ ];
   constructor() { }
 
   ngOnInit() {
